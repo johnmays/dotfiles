f8362f5d495dfa4eb7f9fdcc19a058e74071e578
John Mays <25927827+johnmays@users.noreply.github.com>
2023-11-08 07:14:43 -0500

    keeping track of extensions & shortcuts

--- scripts/update_package_lists.sh
+++ scripts/update_package_lists.sh
@@ -3,13 +3,18 @@
 # This script is just for quickly updating the 
 # package lists that are in my config repo
 
-pacmanpath="$HOME/Projects/config_notes/pacman_packages.txt"
-yaypath="$HOME/Projects/config_notes/yay_packages.txt"
+PACMAN_PATH="$HOME/Projects/config_notes/pacman_packages.txt"
+YAY_PATH="$HOME/Projects/config_notes/yay_packages.txt"
+CODE_EXTENSION_PATH="$HOME/Projects/config_notes/code_extensions.txt"
 
 echo updating pacman packages...
-pacman -Qe > $pacmanpath
+pacman -Qe > $CODE_EXTENSION_PATH
 echo finished updating pacman
 
 echo updating yay packages...
-yay -Qe > $yaypath
+yay -Qe > $YAY_PATH
 echo finished updating yay
+
+echo updating VSCode extensions...
+code --list-extensions > $CODE_EXTENSION_PATH
+echo finished updating code extensions
